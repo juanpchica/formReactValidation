83dee661f3be087ec2fd3d83725d94668c9ce3cb
juanpchica <juanpchica@hotmail.com>
2021-04-22 16:48:22 -0500

    Url format validation added

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
     const letters = /^[A-Za-z]+$/;
     const numbers = /^[0-9]+$/;
     const mailformat = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    const urlBlogFormat = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
 
     switch (name) {
       case "name":
@@ -38,6 +39,13 @@ function App() {
           result = false;
         }
         break;
+      case "url":
+        if (!value.match(urlBlogFormat)) {
+          result = true;
+        } else {
+          result = false;
+        }
+        break;
     }
 
     return result;
@@ -48,7 +56,8 @@ function App() {
     if (
       !inputCheck("name", data.name) &&
       !inputCheck("phone", data.phone) &&
-      !inputCheck("email", data.email)
+      !inputCheck("email", data.email) &&
+      !inputCheck("url", data.url)
     ) {
       showMessage(true, "Form is Complete!", "success");
     } else {
